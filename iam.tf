55ca602aba8adb5baf7af1d402ae75b6ffc74194
Wassimrkik <rmwassim@gmail.com>
2025-07-04 10:16:54 +0000

    used appropriate service account for run services

---
+++ iam.tf
@@ -0,0 +1,52 @@
+# resource "google_cloud_run_service_iam_policy" "private" {
+#   location    = var.region
+#   project     = var.project_id
+#   service     = "be-poc-cori"
+#   policy_data = data.google_iam_policy.private.policy_data
+#   depends_on  = [module.cori-be]
+# }
+data "google_iam_policy" "private" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "serviceAccount:${google_service_account.default.email}",
+    ]
+  }
+}
+
+resource "google_service_account" "default" {
+  depends_on   = [module.cori-be]
+  account_id   = "cloud-run-interservice-id"
+  description  = "Identity used by a public Cloud Run service to call private Cloud Run services."
+  display_name = "cloud-run-interservice-id"
+}
+
+resource "google_project_iam_member" "grant_secret_accessor_public" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+# resource "google_project_iam_member" "grant_secret_accessor" {
+#   project = var.project_id
+#   role    = "roles/secretmanager.secretAccessor"
+#   member  = "serviceAccount:${google_service_account.secret-accessor.email}"
+# }
+
+# resource "google_service_account" "secret-accessor" {
+#   account_id   = "secret-accessor"
+#   description  = "Identity used to access secrets"
+#   display_name = "secret-accessor"
+# }
+
+resource "null_resource" "wait_for_iam_propagation" {
+  provisioner "local-exec" {
+    command = "sleep 15"
+  }
+
+  triggers = {
+    always = timestamp() # ensures it runs every time
+  }
+
+  depends_on = [google_project_iam_member.grant_secret_accessor_public]
+}
--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "cori-be" {
   database_name     = "be-sql"
   lb_name           = "api"
   domain            = "api.${var.domain}"
-  service-account   = google_service_account.secret-accessor.email
+  service-account   = google_service_account.default.email
   sql_password      = var.PGPASSWORD
   env_file_override = "${path.module}/envs/env_be.json"
   secrets           = local.be_service_secrets
@@ -54,56 +54,3 @@ module "cori-addin" {
   container_port    = 443
 }
 
-data "google_iam_policy" "private" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "serviceAccount:${google_service_account.default.email}",
-    ]
-  }
-}
-
-# resource "google_cloud_run_service_iam_policy" "private" {
-#   location    = var.region
-#   project     = var.project_id
-#   service     = "be-poc-cori"
-#   policy_data = data.google_iam_policy.private.policy_data
-#   depends_on  = [module.cori-be]
-# }
-
-resource "google_service_account" "default" {
-  depends_on   = [module.cori-be]
-  account_id   = "cloud-run-interservice-id"
-  description  = "Identity used by a public Cloud Run service to call private Cloud Run services."
-  display_name = "cloud-run-interservice-id"
-}
-
-resource "google_project_iam_member" "grant_secret_accessor_public" {
-  project = var.project_id
-  role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:${google_service_account.default.email}"
-}
-
-resource "google_project_iam_member" "grant_secret_accessor" {
-  project = var.project_id
-  role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:${google_service_account.secret-accessor.email}"
-}
-
-resource "google_service_account" "secret-accessor" {
-  account_id   = "secret-accessor"
-  description  = "Identity used to access secrets"
-  display_name = "secret-accessor"
-}
-
-resource "null_resource" "wait_for_iam_propagation" {
-  provisioner "local-exec" {
-    command = "sleep 15"
-  }
-
-  triggers = {
-    always = timestamp() # ensures it runs every time
-  }
-
-  depends_on = [google_project_iam_member.grant_secret_accessor_public]
-}
